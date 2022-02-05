5c470199c8f75d638a15a62ec0df8cf9bb813a1c
21kkfy <21kkfy@gmail.com>
2022-02-05 17:21:44 +0300

    Updated CardItem.js
    Fixed the bug with CardItems not being in a box like they were before.

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function CardItem(props) {
   return (
     <>
-      <li className='cards__item'>
+      <li className='cards__item__link'>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
               className='cards__item__img'
