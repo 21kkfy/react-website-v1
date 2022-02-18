af6e41dbe9eb634fcb26942072996423d2b6d139
21kkfy <21kkfy@gmail.com>
2022-02-19 00:33:08 +0300

    removed unnecessary comments

--- src/components/pages/ConnectWallet.js
+++ src/components/pages/ConnectWallet.js
@@ -10,15 +10,7 @@ async function ConnectWallet() {
       alert("Get MetaMask!");
       return;
     }
-
-    /*
-    * Fancy method to request access to account.
-    */
     const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-
-    /*
-    * Boom! This should print out public address once we authorize Metamask.
-    */
     console.log("Connected", accounts[0]);
   } catch (error) {
     console.log(error)
