e2cfd4b392fcfe5c1b9224980678d4c3f5a6e563
21kkfy <21kkfy@gmail.com>
2022-02-20 19:21:39 +0300

    RoadMap leftovers removed, icons removed, .gitignore updated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import RoadMap from './components/pages/RoadMap';
+//import RoadMap from './components/pages/RoadMap';
 import Mint from './components/pages/Mint';
 import './styles/App.css';
 
+//<Route path='/road-map' component={RoadMap} />
 function App() {
   return (
     <>
@@ -13,7 +14,6 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/road-map' component={RoadMap} />
           <Route path='/mint' component={Mint} />
         </Switch>
       </Router>
