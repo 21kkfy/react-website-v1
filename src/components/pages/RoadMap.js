86c1f82be113f2a49d181b2071b9e8a3fd69da30
21kkfy <21kkfy@gmail.com>
2022-02-28 13:14:26 +0300

    added road-map id to roadmap section

--- src/components/pages/RoadMap.js
+++ src/components/pages/RoadMap.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function RoadMap() {
   return (
     <>
-    <h1 className='road-map reveal'>Road Map TBA</h1>
+      <h1 id='road-map' className='road-map reveal'>Road Map TBA</h1>
     </>
   );
 }
